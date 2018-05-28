389be3743acd56b3265908620e6935b814b343e7
Taha-Yassine Moslih <anahonawalastohonak@gmail.com>
2018-05-28 03:22:29 +0100

    Initialize database with a fake data using factories and fake library

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id' => function(){
+            return App\Model\Product::all()->random();
+        },
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => rand(0, 5)
     ];
 });
