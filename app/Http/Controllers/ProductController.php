db76683aa8fa7736d1c267edfc0c22479122802d
Taha-Yassine Moslih <anahonawalastohonak@gmail.com>
2018-06-01 18:03:07 +0100

    delete product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -119,6 +119,10 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response()->json([
+            'status' => '200 ok',
+            'descreption' => 'product deleted success'
+        ], 204);
     }
 }
